15e129169be3dab707f63df27740ee9009ab8dd2
Clément Giraud-Sauveur <git@milouki.ovh>
2025-02-21 13:34:41 +0100

    Question 4: ajout de la fonction qui permet de vérifier si le joueur a gagné

--- main.c
+++ main.c
@@ -4,30 +4,41 @@
 int tailleMot(char motSecret[])
 {
     int nombreLettres = 0;
-    for(int i = 0; motSecret[i] != '\0'; i++)
-    {
+    for (int i = 0; motSecret[i] != '\0'; i++)
         nombreLettres++;
-    }
+
     return nombreLettres;
 }
 
 void initTableau(int *lettreTrouvee, int taille)
 {
-    for(int i = 0; i < taille; i++)
-    {
+    for (int i = 0; i < taille; i++)
         lettreTrouvee[i] = 0;
+}
+
+int gagne(int *lettreTrouvee, int nombreLettres)
+{
+    int i = 0;
+    int joueurGagne = 1;
+
+    for (i = 0; i < nombreLettres; i++)
+    {
+        if (lettreTrouvee[i] == 0)
+            joueurGagne = 0;
     }
+
+    return joueurGagne;
 }
 
 int main(int argc, char *argv[])
 {
-    char lettre = 0; // Stocke la lettre proposée par l'utilisateur (retour du scanf)
+    char lettre = 0;            // Stocke la lettre proposée par l'utilisateur (retour du scanf)
     char motSecret[] = "ROUGE"; // C'est le mot à trouver
-    int coupsRestants = 10; // Compteur de coups restants (0 = mort)
+    int coupsRestants = 10;     // Compteur de coups restants (0 = mort)
     int nombreLettres = tailleMot(motSecret);
-    int *lettreTrouvee = NULL;
+    int *lettreTrouvee = NULL; // Tableau de booleens
     lettreTrouvee = malloc(nombreLettres * sizeof(int));
     initTableau(lettreTrouvee, nombreLettres);
-    
+
     return 0;
 }
